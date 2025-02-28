7d49f2c360f659ec64a1a2d9010e87761b041b52
Maharshi Patel <maharship@jfrog.com>
2025-02-28 13:50:00 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Sample .NET Console Application with Serilog
+using System;
+using Serilog;
+
+class Program
+{
+    static void Main()
+    {
+        // Configure Serilog
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Information("Hello, .NET with Serilog!");
+
+        Log.CloseAndFlush();
+    }
+}
